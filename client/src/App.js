4f260ed71cfc749ac604de393d0a60046275acef
dogueone <dncrage@hotmail.com>
2021-03-05 20:05:06 +0300

    Context for login

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useState, useCallback } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
+import { AuthContext } from "./context/auth-context";
 import EditProductPage from "./pages/EditProductPage";
 import AuthPage from "./pages/AuthPage";
 import ProductsPage from "./pages/Products";
@@ -9,35 +10,54 @@ import NewProductPage from "./pages/NewProductPage";
 import MainNavigation from "./components/Navigation/MainNavigation";
 import ProductPage from "./pages/ProductPage";
 
-// <Redirect from='/' to='/auth' exact />
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-class App extends Component {
-  render() {
-    return (
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path="/" component={ProductsPage} exact />
+        <Route path="/books/add-product" component={NewProductPage} exact />
+        <Route path="/books/edit-product/:bid" component={EditProductPage} />
+        <Route path="/books/:bid" component={ProductPage} />
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" component={ProductsPage} exact />
+        <Route path="/auth" component={AuthPage} exact />
+        <Route path="/books/add-product" component={NewProductPage} exact />
+        <Route path="/books/edit-product/:bid" component={EditProductPage} />
+        <Route path="/books/:bid" component={ProductPage} />
+        <Redirect to="/" />
+      </Switch>
+    );
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
       <BrowserRouter>
         <Fragment>
           <MainNavigation />
-          <main className="main-content all-center">
-            <Switch>
-              <Route path="/" component={ProductsPage} exact />
-              <Route path="/auth" component={AuthPage} exact />;
-              <Route
-                path="/books/add-product"
-                component={NewProductPage}
-                exact
-              />
-              <Route
-                path="/books/edit-product/:bid"
-                component={EditProductPage}
-              />
-              <Route path="/books/:bid" component={ProductPage} />
-              <Redirect to="/" />
-            </Switch>
-          </main>
+          <main className="main-content all-center">{routes}</main>
         </Fragment>
       </BrowserRouter>
-    );
-  }
-}
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
--- client/src/components/Navigation/NavLinks.js
+++ client/src/components/Navigation/NavLinks.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
+import { AuthContext } from "../../context/auth-context";
 import "./NavLinks.css";
 
 const NavLinks = () => {
+  const auth = useContext(AuthContext);
   return (
     <ul className="nav-links">
       <li>
@@ -14,9 +16,16 @@ const NavLinks = () => {
       <li>
         <NavLink to="/books/add-product">Add Product</NavLink>
       </li>
-      <li>
-        <NavLink to="/auth">Log In</NavLink>
-      </li>
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">Log In</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
+        </li>
+      )}
     </ul>
   );
 };
