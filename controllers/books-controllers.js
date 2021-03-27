5972bf6ab890d338c9c0dd695bc84d4db62f46a3
dogueone <dncrage@hotmail.com>
2021-03-27 15:24:11 +0300

    CRUD Product Model

--- controllers/books-controllers.js
+++ controllers/books-controllers.js
@@ -1,6 +1,8 @@
-const HttpError = require("../models/http-error");
 const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
+
+const HttpError = require("../models/http-error");
+const Product = require("../models/product");
 
 let BOOKS = [
   {
@@ -32,78 +34,129 @@ let BOOKS = [
   },
 ];
 
-const getBooks = (req, res, next) => {
-  if (BOOKS.length < 1) {
-    throw new HttpError("Could not find any books", 404);
+const getBooks = async (req, res, next) => {
+  let books;
+  try {
+    books = await Product.find();
+  } catch (err) {
+    return next(
+      new HttpError("Fetching book failed, please try again later", 500)
+    );
   }
-  res.json(BOOKS);
+  if (books.length === 0) {
+    return next(new HttpError("Could not find any books", 404));
+    // throw new HttpError("Could not find any books", 404);
+  }
+
+  res.json({ books: books.map((book) => book.toObject({ getters: true })) });
 };
 
-const getBookById = (req, res, next) => {
+const getBookById = async (req, res, next) => {
   const bookId = req.params.bid;
-
-  const book = BOOKS.find((book) => bookId === book.id);
-
-  if (!book) {
-    throw new HttpError("Could not find a book for the provided id", 404);
-
-    // return next(
-    //   new HttpError("Could not find a book for the provided id", 404)
-    // );
+  let book;
+  try {
+    book = await Product.findById(bookId);
+  } catch (err) {
+    return next(
+      new HttpError("Fetching book failed, please try again later", 500)
+    );
   }
-  res.json({ book }); // {book: book}
+  if (!book) {
+    return next(
+      new HttpError("Could not find a book for the provided id", 404)
+    );
+    // throw new HttpError("Could not find a book for the provided id", 404);
+  }
+  res.json({ book: book.toObject({ getters: true }) });
 };
 
-const createBook = (req, res, next) => {
+const createBook = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     console.log(errors);
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
 
-  const { title, description, image, price } = req.body;
-  const createdBook = {
-    id: uuidv4(),
+  const { title, description, image, price, creator } = req.body;
+
+  const createdBook = new Product({
     title,
     image,
     description,
     price,
-  };
-  BOOKS.unshift(createdBook);
-  res.status(201).json({ book: createdBook });
+    creator,
+  });
+
+  try {
+    await createdBook.save();
+  } catch (err) {
+    const error = new HttpError("Creating book failed, please try again.", 500);
+    return next(error);
+  }
+  res.status(201).json({ createdBook: createdBook });
 };
 
-const updateBook = (req, res, next) => {
+const updateBook = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     console.log(errors);
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
 
   const { title, description, price } = req.body;
   const bookId = req.params.bid;
 
-  const updatedBook = { ...BOOKS.find((b) => b.id === bookId) };
-  const updatedBookIndex = BOOKS.findIndex((b) => b.id === bookId);
+  let updatedBook;
+  try {
+    updatedBook = await Product.findById(bookId);
+  } catch (err) {
+    return next(
+      new HttpError("Fetching book failed, please try again later", 500)
+    );
+  }
 
   updatedBook.title = title;
   updatedBook.description = description;
   updatedBook.price = price;
 
-  BOOKS[updatedBookIndex] = updatedBook;
-  res.status(200).json({ book: updatedBook });
-};
-
-const deleteBook = (req, res, next) => {
-  const bookId = req.params.bid;
-
-  if (!BOOKS.find((b) => b.id === bookId)) {
-    throw new HttpError("Could not find book for that id.", 404);
+  try {
+    await updatedBook.save();
+  } catch (err) {
+    return next(
+      new HttpError("Something went wrong, could not update the book", 500)
+    );
   }
 
-  BOOKS = BOOKS.filter((b) => b.id !== bookId);
+  res
+    .status(200)
+    .json({ updatedBook: updatedBook.toObject({ getters: true }) });
+};
+
+const deleteBook = async (req, res, next) => {
+  const bookId = req.params.bid;
+  let book;
+  try {
+    book = await Product.findById(bookId);
+  } catch (err) {
+    return next(
+      new HttpError("Fetching book failed, please try again later", 500)
+    );
+  }
+
+  try {
+    book = await book.remove();
+  } catch (err) {
+    return next(
+      new HttpError("Fetching book failed, please try again later", 500)
+    );
+  }
+
   res.status(200).json({ message: "Deleted place." });
 };
 
--- models/product.js
+++ models/product.js
@@ -1,29 +1,26 @@
-const mongoose = require(mongoose);
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
-
-const productSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    imageUrl: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    } 
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  creator: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
